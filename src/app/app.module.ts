1cc9794854d2d8a4f6b9a6a0f6d5583a5ea96a14
fran-valls <fjvalls@gmail.com>
2023-06-20 14:25:07 +0200

    commint componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {SharedModule} from "primeng/api";
+import {PrimengModule} from "./primeng/primeng.module";
+import {PreguntasModule} from "./preguntas/preguntas.module";
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    PrimengModule,
+    PreguntasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+
+
+
+@NgModule({
+  declarations: [
+
+    MenuComponent
+  ],
+  exports: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
