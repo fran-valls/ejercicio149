6dd745f26614563be3b7732df5b1e3b35775bdef
Java <fjvalls@gmail.com>
2023-06-21 01:06:49 +0200

    Ejercicio hasta routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./preguntas/components/home/home.component";
+import {CapitalesComponent} from "./preguntas/components/capitales/capitales.component";
+import {BanderasComponent} from "./preguntas/components/banderas/banderas.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent, pathMatch: "full"},
+  {path: "preguntas/home", component: HomeComponent},
+  {path: "preguntas/capitales", component: CapitalesComponent},
+  {path: "preguntas/banderas", component: BanderasComponent},
+  {path: "**", component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import {SharedModule} from "primeng/api";
 import {PrimengModule} from "./primeng/primeng.module";
 import {PreguntasModule} from "./preguntas/preguntas.module";
-import { AppRoutingModule } from './app-routing.module';
+import {SharedModule} from "./shared/shared.module";
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
     PrimengModule,
-    PreguntasModule
+    PreguntasModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
